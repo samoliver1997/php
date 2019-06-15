38b9b9543040c8ff36d6aa741bf2d104b918299e
samoliver1997 <samuel.jso18@gmail.com>
2019-06-15 16:16:03 -0300

    1506

--- variaveis/exemplo03.php
+++ variaveis/exemplo03.php
@@ -1,7 +1,21 @@
 <?php
 
 $nome = "hcode";
-$site = "www"
+$site = 'www.hcode.com.br';
+$ano = 1990;
+$salario = 5500.99;
+$bloqueado = false;
 
+//////////////////Variaveis compostas//////////////////
+//echo $frutas[2];
+//echo "</br>";
+$nascimento = new DateTime();
+//var_dump($nascimento);
 
+/////////////////////////Especiais////////////////////////////
+$arquivo = fopen("exemplo03.php","r");
+///var_dump($arquivo);
+
+$nulo = NULL;
+$vazio = "";
 ?>
---
+++ variaveis/exemplo04.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = (int)$_GET["a"];
+
+///var_dump($nome);
+
+$ip = $_SERVER["SCRIPT_NAME"];
+
+echo $ip;
+
+
+
+?>
