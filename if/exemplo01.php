0872a26c5d0eecff63bb285444a56622b683733a
Samuel Oliveira <samuel.jso18@gmail.com>
2019-09-12 11:40:26 -0300

    Update 12092019

--- if/exemplo01.php
+++ if/exemplo01.php
@@ -1,16 +1,21 @@
 <?php
 
-$qualasuaidade = 30;
+$qualasuaidade = 15;
 $idadecrianca = 12;
 $idademaior = 18;
 $idademelhor = 65;
 
 
 if ($qualasuaidade < $idadecrianca) {
-
-	echo "Criança";
-
+	echo "Criança!";
+} elseif ($qualasuaidade > $idademaior) {
+	echo "Adolescente";
+} elseif ($qualasuaidade < $idademelhor ) {
+	echo "Adulto";
+} else {
+	echo "Idoso";
 }
-
+echo "<br>";
+echo ($qualasuaidade < $idademaior)?"Menor de Idade":"Maior de Idade";
 
 ?>
---
+++ switch/exemplo01.php
@@ -0,0 +1,32 @@
+<?php
+
+$diadasemana = date("w");
+
+switch ($diadasemana) {
+	case '0':
+		echo "Domingo";
+		break;
+	case '1':
+		echo "Segunda-feira";
+		break;
+	case '2':
+		echo "Terça-feira";
+		break;
+	case '3':
+		echo "Quarta-feira";
+		break;
+	case '4':
+		echo "Quinta-feira";
+		break;
+	case '5':
+		echo "Sexta-feira";
+		break;		
+	case '6':
+		echo "Sábado";
+		break;
+	default:
+		echo "Data Inválida";
+		break;	
+}
+
+?>
